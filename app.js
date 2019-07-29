ff775cce8e8beea6642b8b25a50f42fd9d79833e
zboyle <zboyle@umich.edu>
2019-07-28 21:19:14 -0400

    now hosting on heroku

--- app.js
+++ app.js
@@ -39,6 +39,6 @@ app.get('*', (req, res) => {
 });
 
 // listen on port 3000
-app.listen(3000, () => {
-  console.log('serving on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('serving chrs webapp');
 });
